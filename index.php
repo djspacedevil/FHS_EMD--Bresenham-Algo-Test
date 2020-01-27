c81e73e171d141764df314fac67ae631df19013c
djspacedevil <goesi@emsland-party.de>
2020-01-27 21:00:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     #$x2 = rand(1,500);
     #$y2 = rand(1,500);
 
-    $x1 = 63; //63
-    $y1 = 111; //111
+    $x1 = 10; //63
+    $y1 = 10; //111
 
-    $x2 = -67; //-67
-    $y2 = 53; //53
+    $x2 = 5; //-67
+    $y2 = 2; //53
 
     echo "Alt x: " . $x1 . " y: " . $y1 .PHP_EOL;
     echo "Neu x: " . $x2 . " y: " . $y2 .PHP_EOL.PHP_EOL;
@@ -31,12 +31,12 @@
         echo $str_delta_y = "(y2 > y1) (y2 größer y1) daher iy = 1 | DELTA y = 2*(y2 - y1)  = ".$delta_y." = 2*(".$y2." - ".$y1.")".PHP_EOL;
     } else {
         $iy = -1; $delta_y = ($y1 - $y2) << 1;
-        echo $str_delta_y = "(y2 < y1) (y2 kleiner y1) daher ix = -1 | DELTA y = 2*(y1 - y2)  = ".$delta_y." = 2*(".$y1." - ".$y2.")".PHP_EOL;
+        echo $str_delta_y = "(y2 < y1) (y2 kleiner y1) daher iy = -1 | DELTA y = 2*(y1 - y2)  = ".$delta_y." = 2*(".$y1." - ".$y2.")".PHP_EOL;
     }
 
     echo "Start-Punkt x: " . $x1 . " y: " . $y1 . PHP_EOL;
 
-    echo "DELTA X:".$delta_x. " DELTA Y:".$delta_y.PHP_EOL;
+    echo "DELTA X:".$delta_x. " DELTA Y:".$delta_y.' | Schnelle Richtung: '.(($delta_x >= $delta_y)?'x':'y').PHP_EOL;
 
     $line = 2;
     if ($delta_x >= $delta_y) {
